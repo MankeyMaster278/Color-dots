4023ed011858239d80c614157fc2480d8a7a0347
Sean <Smlyn704@gmail.com>
2017-07-18 17:31:21 -0500

    hover

--- cd.js
+++ cd.js
@@ -18,27 +18,32 @@ i = i + 2;
 
 var me = document.querySelector('body');
 
+var borderTypes = ['solid', 'dashed', 'dotted'];
+
 
 
 var counter = 0;
 
-while (counter < 5) {
+while (counter < 16) {
 var you = document.createElement('div');
 
 
-var purpleValue = getRandomInt(0,255);
-var orangeValue = getRandomInt(0,255);
-var yellowValue = getRandomInt(0,255);
+var purpleBackground = getRandomInt(0,255);
+var orangeBackground = getRandomInt(0,255);
+var yellowBackground = getRandomInt(0,255);
+var alphaBackground = getRandomArbitrary(0.5,1);
 
-you.style.backgroundColor = `rgb(${purpleValue}, ${orangeValue}, ${yellowValue})`;
+you.style.backgroundColor = `rgba(${purpleBackground}, ${orangeBackground}, ${yellowBackground}, ${alphaBackground})`;
 
 
 
 var purpleBorder = getRandomInt(0,255);
 var orangeBorder = getRandomInt(0,255);
 var yellowBorder = getRandomInt(0,255);
+var alphaBorder = getRandomArbitrary(0.5,1);
 
-you.style.borderColor = `rgb(${purpleBorder}, ${orangeBorder}, ${yellowBorder}`;
+you.style.borderColor = `rgba(${purpleBorder}, ${orangeBorder}, ${yellowBorder}, ${alphaBorder})`;
+you.style.borderStyle = borderTypes[getRandomInt(0,2)]
 
 
 
@@ -54,6 +59,25 @@ counter += 1;
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var divs = document.querySelectorAll("div");
+
+divs.forEach(function(div){
+
+    div.addEventListener('mouseover', function(){
+        
+    var blueBackground = getRandomInt(0,255);
+    var greenBackground = getRandomInt(0,255);
+    var redBackground = getRandomInt(0,255);
+    var alphaBackground = getRandomArbitrary(0.5,1);
+    div.style.backgroundColor = `rgba(${blueBackground}, ${greenBackground}, ${redBackground}, ${alphaBackground})`;
+    })
+});
+
+
 
 
 
